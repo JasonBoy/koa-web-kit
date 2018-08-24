fe487d9234878f5b9ce2d0afdf7a631a8ed5da4a
Jason <jasonlikenfs@gmail.com>
2018-08-24 11:55:40 +0800

    :two_men_holding_hands:Clean up deploy script & rename to pm2.config.js

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 #This is meant for production
 # > ./deploy.sh skipInstall skipBuild skipServer
 
-#using node >= 7.6
+##Using node >= 8
 #nvm use 8
 #source $HOME/.nvm/nvm.sh; nvm use 8
 
@@ -21,61 +21,48 @@ else
 #  echo yarn/$(yarn -v)
 fi
 
-#remove the freaking package-lock.json
+##Remove the freaking package-lock.json
 #rm -f package-lock.json
 
-#uncoment this if you are in China...
+##=====Uncomment script below if you are in China=====
 #TaobaoRegistry="http://registry.npm.taobao.org/"
 #NpmRegistry=$(npm config get registry)
-#npm config set registry ${TaobaoRegistry}
 #if [ "$TaobaoRegistry" != "$NpmRegistry" ]; then
-#  echo changing npm registry to taobao registry "$TaobaoRegistry"
+#  echo changing npm registry to taobao registry: "$TaobaoRegistry"
 #  npm config set registry "$TaobaoRegistry"
 #fi
-
+##Change SASS binary site to taobao
 #export SASS_BINARY_SITE=https://npm.taobao.org/mirrors/node-sass/
 
-#installing npm modules
+##Installing npm modules
 if [[ $1 != "1" ]]; then
   echo installing npm modules...
+  ##Don't install based on the package-lock.json by default, instead, only refer to package.json
   npm install --no-shrinkwrap
 #  yarn install --production=false
 fi
 
 if [[ $2 != "1" ]]; then
-#webpack is bundling modules
+##webpack is bundling modules
 echo webpack is bundling modules...
 npm run build
 npm run ssr
-echo ===build finished===
+echo =====Build Finished=====
 #yarn run prod
 fi
 
-#if skipBuild is false
+##Start server with pm2
 if [[ $3 != "1" ]]; then
   export NODE_ENV=production
 
-  PMVersion=$(pm2 -v)
+  PM2Version=$(pm2 -v)
   if [[ $? != 0 ]]; then
     echo ERROR: pls install pm2 to continue...
     exit
-  #  echo installing pm2...
-  #  npm install -g pm2
-  fi
-
-  ClientScript="app.js"
-  #For just make it to ClientScript
-  RunScript=${ClientScript}
-  ClusterNumber=0
-  if [[ $2 != "" ]]; then
-    ClusterNumber=$2
   fi
 
   echo NODE_ENV: ${NODE_ENV}
-  echo Using ${RunScript}
+  echo "Using pm2: [$(which pm2)]"
 
-  echo "Using pm2 [$(which pm2)]"
-
-  pm2 reload ecosystem.config.js --update-env --env production
-
+  pm2 reload pm2.config.js --update-env --env production
 fi
