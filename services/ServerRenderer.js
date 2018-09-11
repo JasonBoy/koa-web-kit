83b23d7987814484f71ec804612c5c03295b6111
Jason <jasonlikenfs@gmail.com>
2018-09-11 17:39:48 +0800

    :wrench:Remove ssr log when its disabled

--- services/ServerRenderer.js
+++ services/ServerRenderer.js
@@ -89,7 +89,7 @@ class ServerRenderer {
     this.ssrEnabled = options.hasOwnProperty('ssr')
       ? !!options.ssr
       : isSSREnabled;
-    this.cache = options.cache || new Cache(options);
+    this.cache = options.cache || (this.ssrEnabled ? new Cache(options) : null);
     this.streaming = !!options.streaming;
   }
 
@@ -195,7 +195,7 @@ class ServerRenderer {
     </html>
   `;
 
-    this.cache.set(ctx.path, ret);
+    this.cache && this.cache.set(ctx.path, ret);
     return ret;
   }
 
@@ -296,7 +296,7 @@ class ServerRenderer {
    * @return {boolean}
    */
   isCacheMatched(key) {
-    if (this.cache.has(key)) {
+    if (this.cache && this.cache.has(key)) {
       logger.info(`Cache for [${key}] matched!`);
       return true;
     }
