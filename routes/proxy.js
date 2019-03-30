fbbceef1d682a4e9b9bf9eefac1ddeaa0c572019
jason <jasonlikenfs@gmail.com>
2019-03-30 22:43:49 +0800

    :bug:Fix #31, stream interrupted in koa

--- routes/proxy.js
+++ routes/proxy.js
@@ -2,6 +2,7 @@
  * Request proxy for backend api
  */
 
+const { PassThrough } = require('stream');
 const Router = require('koa-router');
 
 const { HttpClient } = require('../services/HttpClient');
@@ -11,8 +12,17 @@ exports.handleApiRequests = function(prefix, endPoint) {
   // TODO: a path rewrite?
   const router = new Router({ prefix: routerPrefix });
   const apiProxy = new HttpClient({ endPoint, prefix });
-  router.all('*', ctx => {
-    ctx.body = apiProxy.proxyRequest(ctx);
+  router.all('*', async ctx => {
+    const requestStream = apiProxy.proxyRequest(ctx);
+    const pt = requestStream.pipe(PassThrough());
+    await new Promise(resolve => {
+      requestStream.on('response', response => {
+        ctx.status = response.statusCode;
+        ctx.set(response.headers);
+        resolve();
+      });
+    });
+    ctx.body = pt;
   });
   return router.routes();
 };
