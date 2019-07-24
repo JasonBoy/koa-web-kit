4e22713dd19a12d53a8da58f0bb861e8f6dce43e
jason <jasonlikenfs@gmail.com>
2019-07-25 00:13:23 +0800

    minor

--- routes/index.js
+++ routes/index.js
@@ -47,32 +47,6 @@ router.post(
 /**
  * Async request data for initial state demo
  */
-router.get('/github', async function(ctx) {
-  if (renderer.isCacheMatched(ctx.path)) {
-    renderer.renderFromCache(ctx.path, ctx);
-    return;
-  }
-
-  if (!renderer.isSSREnabled()) {
-    ctx.body = renderer.genHtml('', {}, ctx);
-    return;
-  }
-
-  //you can use isomorphic-fetch to share the fetch logic
-  logger.info('requesting github data...');
-  const res = await got(
-    'https://api.github.com/repos/jasonboy/wechat-jssdk/branches',
-    {
-      headers: {
-        'User-Agent':
-          'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
-      },
-      json: true,
-    }
-  );
-  const data = { github: res.body };
-  renderer.render(ctx, data);
-});
 
 router.get('/400', async ctx => {
   ctx.status = 400;
