a9e045acf8898f6e4d8f5f66ea84f1c6d0bfb3a5
jason <jasonlikenfs@gmail.com>
2021-08-16 13:51:36 +0800

    :bug: Add optional chaining plugin since webpack4 can't handle it

--- babel.config.js
+++ babel.config.js
@@ -60,6 +60,8 @@ module.exports = function (api) {
           displayName: DEV_MODE,
         },
       ],
+      '@babel/plugin-proposal-optional-chaining',
+      '@babel/plugin-proposal-nullish-coalescing-operator',
       '@babel/plugin-transform-runtime',
       '@babel/plugin-proposal-object-rest-spread',
       '@babel/plugin-proposal-class-properties',
